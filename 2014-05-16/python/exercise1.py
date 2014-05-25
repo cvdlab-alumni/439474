d22139b28ba363626895fbcf30f7d331527cd9f8
3+4 <marzi91@hotmail.it>
2014-05-25 16:56:47 +0200

    add balcon at home

--- 2014-05-16/python/exercise1.py
+++ 2014-05-16/python/exercise1.py
@@ -13,153 +13,190 @@ from architectural import *
 from myfont import *
 from sysml import *
 
+#unico blocco iniziale 
 master_block = assemblyDiagramInit([3,3,3])([[0.4,10.7,0.4],[0.4,12.6,0.4],[0.3,2.7,0.3]])
 DRAW = COMP([VIEW,STRUCT,MKPOLS])
 V,CV = master_block
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(CV)),CYAN,2)
-VIEW(hpc)
+#VIEW(hpc)
 
+#divisione tra sala corridoio cucina camera mia e balcone scale, cameretta, camera mamma 
 toMerge = 13
 split = assemblyDiagramInit([3,1,1])([[5.5,0.3,5],[12.6],[2.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,2)
-VIEW(hpc)
+#VIEW(hpc)
 
+#divisione tra sala camera mia ed uno spazio comprensivo di corridoio bagno e cucina 
 toMerge = 26
 split = assemblyDiagramInit([1,5,1])([[5.5],[4,0.2,4.7,0.2,3.5],[2.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,2)
-VIEW(hpc)
+#VIEW(hpc)
 
+#divisione tra balcone buco delle scale cameretta e camera di mamma
 toMerge = 27
 split = assemblyDiagramInit([1,7,1])([[5],[1.3,0.2,2.5,0.2,3.4,0.2,4.8],[2.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,2)
-VIEW(hpc)
+#VIEW(hpc)
 
+#divisione tra corridoio e lo spazio con bagno e cucina
 toMerge = 29
 split = assemblyDiagramInit([3,1,1])([[3.5,0.2,1.3],[4.7],[2.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,2)
-VIEW(hpc)
+#VIEW(hpc)
 
+#divisione tra bagno e cucina
 toMerge = 38
 split = assemblyDiagramInit([1,3,1])([[3.5],[2.5,0.2,2],[2.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,2)
-VIEW(hpc)
+#VIEW(hpc)
 
+#per rimuovere il dentro delle camere e il piano superiore 
 toRemove=[27,40,42,30,39,31,33,35,37,13]
 master_block= master_block[0],[cell for k, cell in enumerate (master_block[1]) if not (k in toRemove)]
-DRAW(master_block)
+#DRAW(master_block)
 
 #ora facciamo le porte da 2 metri per 80 cm
+#porta finestra sul balcone, porta della sala,porta della cameretta e porta della camera di mamma
 toMerge = 25
 split = assemblyDiagramInit([1,9,2])([[0.2],[0.3,0.7,1,1.8,0.9,0.8,2.8,0.8,4],[2,0.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,2)
-VIEW(hpc)
+#VIEW(hpc)
 
+#porta della cucina e arco dell'ingresso tra sala cucina e corridoio 
 toMerge = 25
 split = assemblyDiagramInit([5,1,2])([[1.5,0.8,1.85,0.8,0.35],[0.2],[2,0.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,2)
-VIEW(hpc)
+#VIEW(hpc)
 
+#porta camera mia
 toMerge = 25
 split = assemblyDiagramInit([3,1,2])([[4.15,0.8,0.35],[0.2],[2,0.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,2)
-VIEW(hpc)
+#VIEW(hpc)
 
+#porta del bagno
 toMerge = 28
 split = assemblyDiagramInit([1,3,2])([[0.2],[3.3,0.8,0.6],[2,0.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
 #tolgo il balcone
+#divido il lato in due: la parte della sala e del balcone
 toMerge = 10
 split = assemblyDiagramInit([2,1,1])([[5.8,5],[0.4],[2.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
+#divido il cornicione superiore in due: la parte della sala e del balcone
 toMerge = 10
 split = assemblyDiagramInit([2,1,1])([[5.8,5],[0.4],[2.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
+#divido il lato in due: la parte delle camerette e del balcone
 toMerge = 18
 split = assemblyDiagramInit([1,2,1])([[0.4],[1.3,11.3],[2.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
+#divido il cornicione superiore in due: la parte delle camerette e del balcone
 toMerge = 18
 split = assemblyDiagramInit([1,2,1])([[0.4],[1.3,11.3],[2.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
 #ora facciamo le finestre
+#finestra della cucina e finestra del bagno 
 toMerge = 4
-split = assemblyDiagramInit([1,9,3])([[0.4],[1.2,1.6,2.1,1.1,1.5,0.8,1.8,1.5,1],[1,1,0.7]])
+split = assemblyDiagramInit([1,5,3])([[0.4],[4.9,1.1,1.5,0.8,4.4],[1,1,0.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
+#porta finestra della cameretta e della camera di mamma
 toMerge = 69
-split = assemblyDiagramInit([1,7,3])([[0.4],[0.9,1.1,1.9,1.4,2.6,2,1.4],[1,1,0.7]])
+split = assemblyDiagramInit([1,5,2])([[0.4],[4.1,1,4.4,1,0.8],[2,0.7]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
+#portafinestra camera mia
+toMerge = 11
+split = assemblyDiagramInit([3,1,2])([[4.2,1,5.6],[0.4],[2,0.7]])
+master_block = diagram2cell(split,master_block,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
+hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
+#VIEW(hpc)
+
+#finestra sala
+toMerge = 63
+split = assemblyDiagramInit([5,1,3])([[1.9,0.8,0.2,0.8,1.9],[0.4],[1,1,0.7]])
+master_block = diagram2cell(split,master_block,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
+hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
+#VIEW(hpc)
+
+#la cella 9 rappresenta tutto il pavimento e vine suddivisa per ottenere la parte del pavimento del balcone
 toMerge = 9
 split = assemblyDiagramInit([2,3,1])([[5.7,5],[1.5,2.5,8.6],[0.3]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
-toMerge = 64
+#divide il piano del balcone, lato sala, in modo da farlo piu basso
+toMerge = 62
 split = assemblyDiagramInit([1,1,2])([[5],[0.4],[1.2,1.5]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
-toMerge = 66
+#divide il piano del balcone, lato cameretta, in modo da farlo piu basso
+toMerge = 64
 split = assemblyDiagramInit([1,1,2])([[0.4],[1.3],[1.2,1.5]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
-toMerge = 13
+#divide il pilastro del balcone ovvero l'angolo con i fiori, in modo da farlo piu basso
+toMerge = 12
 split = assemblyDiagramInit([1,1,2])([[0.4],[0.4],[1.2,1.5]])
 master_block = diagram2cell(split,master_block,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(master_block)))
 hpc = cellNumbering (master_block,hpc)(range(len(master_block[1])),CYAN,1)
-VIEW(hpc)
+#VIEW(hpc)
 
-toRemove=[24,28,32,36,42,52,58,124,65,13,71,77,83,89,104,110,46,122,119,126,64,125]
+toRemove=[23,27,31,35,41,51,57,63,12,75,69,45,86,106,100,62,122,118,121,120,82,115,92]
 master_block= master_block[0],[cell for k, cell in enumerate (master_block[1]) if not (k in toRemove)]
-DRAW(master_block)
+#DRAW(master_block)
--- 2014-05-16/python/exercise2.py
+++ 2014-05-16/python/exercise2.py
@@ -12,10 +12,9 @@ from scipy import *
 from architectural import *
 from myfont import *
 from sysml import *
-from exercise1 import master_block
+from es1hw3 import master_block
 
 DRAW = COMP([VIEW,STRUCT,MKPOLS])
-block= STRUCT(MKPOLS(master_block))
 
 #modifichiamo il piano terra
 piano_terra=master_block
@@ -24,30 +23,102 @@ hpc = SKEL_1(STRUCT(MKPOLS(piano_terra)))
 hpc = cellNumbering (piano_terra,hpc)(range(len(piano_terra[1])),CYAN,2)
 #VIEW(hpc)
 
-toMerge = 17
+#serve per creare la porta d'ingresso al piano terra
+toMerge = 16
 split = assemblyDiagramInit([3,1,2])([[0.1,1,3.9],[0.2],[2,.7]])
 piano_terra = diagram2cell(split,piano_terra,toMerge)
 hpc = SKEL_1(STRUCT(MKPOLS(piano_terra)))
 hpc = cellNumbering (piano_terra,hpc)(range(len(piano_terra[1])),CYAN,1)
 #VIEW(hpc)
 
-toRemove=[104,103,102,106]
+toRemove=[101,97,98]
 piano_terra= piano_terra[0],[cell for k, cell in enumerate (piano_terra[1]) if not (k in toRemove)]
 #VIEW(hpc)
 
-#provo a chiudere il buco della porta del piano terra
+#chiudo il buco della porta del piano terra
 portachiusa = assemblyDiagramInit([1,1,1])([[0.3],[0.75],[2.7]])
 pchiusa=STRUCT(MKPOLS(portachiusa))
 
 piano_t= STRUCT(MKPOLS(piano_terra))
 piano_t=STRUCT([piano_t,T([1,2])([5.85,0.65])(pchiusa)])
 
-#provo a mettere il pavimento in fondo al primo piano
+#metto il pavimento in fondo al primo piano
 pavchiuso = assemblyDiagramInit([1,1,1])([[5.2],[2.6],[0.3]])
 pvchiuso=STRUCT(MKPOLS(pavchiuso))
 
-piano_t=STRUCT([piano_t,T([1,2])([6,1.8])(pvchiuso)])
+#aggiungo camera da letto di nonna
+camnonna=assemblyDiagramInit([1,1,1])([[6.8],[5],[3.3]])
+camnonna= larApply(t(0,13.4,0))(camnonna)
+hpc = SKEL_1(STRUCT(MKPOLS(camnonna)))
+hpc = cellNumbering (camnonna,hpc)(range(len(camnonna[1])),CYAN,1)
+#VIEW(hpc)
+
+toMerge = 0
+split = assemblyDiagramInit([3,3,2])([[0.4,6,0.4],[0.4,4.2,0.4],[0.3,3]])
+camnonna = diagram2cell(split,camnonna,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(camnonna)))
+hpc = cellNumbering (camnonna,hpc)(range(len(camnonna[1])),CYAN,1)
+#VIEW(hpc)
+
+#facciamo la finestra della camera di nonna
+toMerge = 15
+split = assemblyDiagramInit([1,5,3])([[0.4],[1.2,0.8,0.2,0.8,1.2],[1,1,1]])
+camnonna = diagram2cell(split,camnonna,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(camnonna)))
+hpc = cellNumbering (camnonna,hpc)(range(len(camnonna[1])),CYAN,1)
+#VIEW(hpc)
+
+toRemove=[7,9,21,27]
+camnonna= camnonna[0],[cell for k, cell in enumerate (camnonna[1]) if not (k in toRemove)]
+
+#ora uniamo la camera di nonna al piano terra
+camnonna=STRUCT(MKPOLS(camnonna))
+piano_t=STRUCT([piano_t,camnonna])
 #VIEW(piano_t)
+#fine piano terra
+
+#modifichiamo il primo e il secondo piano nello stesso modo: aggiungiamo i balconi 
+#creiamo il balcone della cameretta e della camera di mamma
+balconecm = assemblyDiagramInit([1,1,1])([[2],[8.6],[1.5]])
+balconecm= larApply(t(11.5,4.8,0))(balconecm)
+hpc = SKEL_1(STRUCT(MKPOLS(balconecm)))
+hpc = cellNumbering (balconecm,hpc)(range(len(balconecm[1])),CYAN,1)
+#VIEW(hpc)
+
+toMerge = 0
+split = assemblyDiagramInit([3,3,2])([[0.4,1.2,0.4],[0.4,7.8,0.4],[0.3,1.2]])
+balconecm = diagram2cell(split,balconecm,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(balconecm)))
+hpc = cellNumbering (balconecm,hpc)(range(len(balconecm[1])),CYAN,1)
+#VIEW(hpc)
+
+toRemove=[3,9]
+balconecm= balconecm[0],[cell for k, cell in enumerate (balconecm[1]) if not (k in toRemove)]
+#DRAW(balconecm)
+#fine del primo balcone
+
+#creiamo il balcone della camera mia
+balconemio = assemblyDiagramInit([1,1,1])([[6.8],[5],[1.5]])
+balconemio= larApply(t(0,13.4,0))(balconemio)
+hpc = SKEL_1(STRUCT(MKPOLS(balconemio)))
+hpc = cellNumbering (balconemio,hpc)(range(len(balconemio[1])),CYAN,1)
+#VIEW(hpc)
+
+toMerge = 0
+split = assemblyDiagramInit([3,3,2])([[0.4,6,0.4],[0.4,4.2,0.4],[0.3,1.2]])
+balconemio = diagram2cell(split,balconemio,toMerge)
+hpc = SKEL_1(STRUCT(MKPOLS(balconemio)))
+hpc = cellNumbering (balconemio,hpc)(range(len(balconemio[1])),CYAN,1)
+#VIEW(hpc)
+
+toRemove=[7,9]
+balconemio= balconemio[0],[cell for k, cell in enumerate (balconemio[1]) if not (k in toRemove)]
+#DRAW(balconemio)
+#fine del secondo balcone
+
+#ora aggrego la casa e i balconi
+secondopiano=evalStruct(Struct([master_block,balconecm,balconemio]))
+secondopiano=STRUCT(CAT(AA(MKPOLS)(secondopiano)))
 
 #ora facciamo le scale
 #pianerottolo
@@ -71,12 +142,14 @@ gradino2=T([1,2,3])([5.2,2.5,1.35])(R([1,2])(PI)(gradino))
 
 #colonna
 column = larApply(t(11.3,0.2,0))(larRod(0.2,3)())
-column=STRUCT(CAT(AA(MKPOLS)(evalStruct(Struct([column])))))
+column=STRUCT(MKPOLS(column))
 
 scala=STRUCT([app2,app,gradino,pian2,rampa2,gradino2])
 scala=STRUCT([scala,T(3)(2.7)(scala),T(3)(5.4)(app)])
 scala=T([1,2])([6.1,1.9])(scala)
-casa=STRUCT([piano_t,T(3)(2.7)(block),T(3)(5.4)(block),scala,column])
+#VIEW(scala)
+
+casa=STRUCT([piano_t,T(3)(2.7)(secondopiano),T(3)(5.4)(secondopiano),scala,column])
 
 #balcone curvo con bezier
 c1=larBezierCurve([[0.84, 0.27], [0.46, 0.18], [0.24, 0.29], [0.22, 0.75]])
@@ -87,10 +160,74 @@ app=PROD([x1,Q(1.2)])
 app2=R([1,2])(PI/2)(app)
 balcone=STRUCT([T([1,2,3])([11.8,-0.3,3])(app2),T([1,2,3])([11.8,-0.3,5.7])(app2)])
 #casa senza tetto
-VIEW(STRUCT([casa,balcone]))
+casa=STRUCT([casa,balcone])
+#VIEW(casa)
+
+#tetto
+#tutto il tetto e' da rifare con V e CV 
+#parte del tetto che sta sopra a sala cucina parte del corridoio  e cameretta
+part1 = assemblyDiagramInit([1,1,1])([[11.9],[9],[0.3]]) 
+p1=STRUCT(MKPOLS(part1))
+p1=R([2,3])(PI/12)(p1)
+
+#parte del tetto che sta sopra la camera mia il bagno e camera di mamma
+p2 = assemblyDiagramInit([1,1,1])([[11.9],[5],[0.3]]) 
+p2=STRUCT(MKPOLS(p2))
+p2=R([2,3])(PI-PI/7)(p2)
+p2=T(2)(12.5)(p2)
+tetto=T(3)(17.3)(STRUCT([p1,p2]))
+#VIEW(tetto)
+tetto=S(3)(0.5)(tetto)
+
+part2 = assemblyDiagramInit([1,1,1])([[11.5],[13.4],[0.1]])
+p3=STRUCT(MKPOLS(part2))
+p3=T(3)(8.6)(p3)
+tetto=JOIN([tetto,p3])
 
 
-#casa con tetto
+
+
+#proviamo a fare una parete con gli archi........
+def circle(r):
+	def circle0(p):
+		alpha = p[0]
+		return [r*COS(alpha), r*SIN(alpha)]
+	return circle0
+
+parete=assemblyDiagramInit([1,1,1])([[0.4],[8.6],[3.3]])
+parete=STRUCT(MKPOLS(parete))
+# tutto il balcone e' 8.6 metto tre colonne ciascuna da 0.4 quindi ottengo un raggio che e' di 1.85
+arco = MAP(circle(1.85))(INTERVALS(PI)(32))
+arco=PROD([Q(0.4),arco])
+arco=T(2)(1.85)(arco)
+c=CUBOID([0.4,1.85,0])
+s=JOIN([c,arco])
+
+s=T(3)(1)(s)
+c1=CUBOID([0.4,3.7,1])
+ss=T(2)(0.4)(STRUCT([s,c1]))
+ss2=T(2)(3.7+0.4)(ss)
+parete=DIFF([parete,ss,ss2])
+parete=T([1,2])([13.1,4.8])(parete)
+#VIEW(parete)
+casa=STRUCT([casa,parete])
+#casa=STRUCT([casa,tetto,parete])
+VIEW(casa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""#casa con tetto
 
 DRAW = COMP([VIEW,STRUCT,MKPOLS])
 block= STRUCT(MKPOLS(master_block))
@@ -181,4 +318,4 @@ tetto=JOIN([tetto,p3])
 
 casa=STRUCT([casa,balcone,tetto])
 
-VIEW(casa)
+#VIEW(casa)"""
